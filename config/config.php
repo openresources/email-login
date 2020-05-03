7b83d528a48984881d58d29a2c57221002f5677e
Eviano Afiemo <6235534+evianoa@users.noreply.github.com>
2020-05-03 13:36:24 +0100

    chore: streamline theme scaffolding

--- config/config.php
+++ config/config.php
@@ -4,5 +4,5 @@
  * You can place your custom package configuration in here.
  */
 return [
-
+    'app_shell_template' => env('APP_SHELL_TEMPLATE', 'email-login::layouts.app_shell'),
 ];
---
+++ resources/views/layouts/scaffold.blade.php
@@ -0,0 +1,13 @@
+@extends(config('email-login.app_shell_template'))
+
+
+@section('scaffold')
+<div class="min-h-screen"
+    style="background:url({{config('app.ui.auth_image_url')}}) no-repeat 50% 90%; background-size: cover;">
+    <section>
+        <div class="container mx-auto py-16">
+            @yield('content')
+        </div>
+    </section>
+</div>
+@endsection
