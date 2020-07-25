3ad43cd3d5cd8c2e9f888ed6aa49aad1e1764b74
Eviano Afiemo <6235534+evianoa@users.noreply.github.com>
2020-07-25 03:36:15 +0100

    refactor: cleanup

--- resources/views/layouts/scaffold.blade.php
+++ resources/views/layouts/scaffold.blade.php
@@ -1,6 +1,5 @@
 @extends(config('email-login.app_shell_template'))
 
-
 @section('scaffold')
 <div class="min-h-screen"
     style="background:url({{config('app.ui.auth_image_url')}}) no-repeat 50% 90%; background-size: cover;">
